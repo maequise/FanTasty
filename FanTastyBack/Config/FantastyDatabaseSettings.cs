7ad20c0d158dddb54c263338aa8eff552b25d7dc
Audrey Serra <audrey.serra@free.fr>
2022-01-18 10:18:48 +0100

    Ajout Config

---
+++ FanTastyBack/Config/FantastyDatabaseSettings.cs
@@ -0,0 +1,18 @@
+﻿namespace FanTastyBack.Config
+{
+    public class FantastyDatabaseSettings : IFantastyDatabaseSettings
+    {
+        public string RecettesCollectionName { get; set; }
+        public string IngredientsCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+
+    public interface IFantastyDatabaseSettings
+    {
+        string RecettesCollectionName { get; set; }
+        string IngredientsCollectionName { get; set; }
+        string ConnectionString { get; set; }
+        string DatabaseName { get; set; }
+    }
+}
