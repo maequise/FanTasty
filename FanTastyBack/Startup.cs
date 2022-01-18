6804549ff2208d15bb038c22891fd36b5579a420
Audrey Serra <audrey.serra@free.fr>
2022-01-18 10:32:01 +0100

    Modifications Startup.cs

--- FanTastyBack/Startup.cs
+++ FanTastyBack/Startup.cs
@@ -1,10 +1,17 @@
+using FanTastyBack.Config;
+using FanTastyBack.Repositories;
+using FanTastyBack.Repositories.Interfaces;
+using FanTastyBack.Services;
+using FanTastyBack.Services.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
@@ -26,7 +33,26 @@ namespace FanTastyBack
         public void ConfigureServices(IServiceCollection services)
         {
 
+            // Configuration DB
+            services.Configure<FantastyDatabaseSettings>(Configuration.GetSection(nameof(FantastyDatabaseSettings)));
+            services.AddSingleton<IFantastyDatabaseSettings>(sp => sp.GetRequiredService<IOptions<FantastyDatabaseSettings>>().Value);
+
+            // Repositories
+            services.AddSingleton<IRecetteRepository, RecetteRepository>();
+            services.AddSingleton<IIngredientRepository, IngredientRepository>();
+            services.AddSingleton<IUtilisateurRepository, UtilisateurRepository>();
+
+            // Services
+            services.AddSingleton<RecetteService, RecetteService>();
+            services.AddSingleton<IngredientService, IngredientService>();
+            services.AddSingleton<UtilisateurService, UtilisateurService>();
+
+            // Controllers
             services.AddControllers();
+
+            // Problèmes de sécurité
+            services.AddCors(o => o.AddPolicy("MyPolicy", builder => { builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader(); }));
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "FanTastyBack", Version = "v1" });
@@ -44,12 +70,17 @@ namespace FanTastyBack
             }
 
             app.UseRouting();
+            app.UseCors("MyPolicy");
 
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapFallback("/", async context =>
+                 {
+                     await context.Response.WriteAsync("Hello World!");
+                 });
             });
         }
     }
