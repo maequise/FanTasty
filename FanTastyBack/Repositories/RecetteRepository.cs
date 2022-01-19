0e344ef5e657f3684d387bdf86b59a69a5ff8344
Audrey Serra <audrey.serra@free.fr>
2022-01-19 17:29:47 +0100

    Ajout des Ingredients au FindAll de Recettes

--- FanTastyBack/Repositories/RecetteRepository.cs
+++ FanTastyBack/Repositories/RecetteRepository.cs
@@ -9,17 +9,26 @@ namespace FanTastyBack.Repositories
     public class RecetteRepository : IRecetteRepository
     {
         private readonly IMongoCollection<Recette> _recettes;
+        private readonly IIngredientRepository _ingredientRepository;
 
-        public RecetteRepository(IFantastyDatabaseSettings settings)
+        public RecetteRepository(IFantastyDatabaseSettings settings, IIngredientRepository ingredientRepository)
         {
             var client = new MongoClient(settings.ConnectionString);
             var database = client.GetDatabase(settings.DatabaseName);
             _recettes = database.GetCollection<Recette>(settings.RecettesCollectionName);
+            _ingredientRepository = ingredientRepository;
         }
 
         public List<Recette> FindAll()
         {
             List<Recette> recettes = this._recettes.Find(rec => true).ToList();
+            for (int i = 0; i < recettes.Count; i++)
+            {
+                foreach (IngredientRecette ingr in recettes[i].Ingredients)
+                {
+                    ingr.Ingredient = this._ingredientRepository.FindById(ingr.Id);
+                }
+            }
             return recettes;
         }
     }
