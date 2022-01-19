5b61626d6a431bab35cbc6f9fdbcf20f7f86e925
Audrey Serra <audrey.serra@free.fr>
2022-01-19 15:27:48 +0100

    Ajout du Model Ingredients et de la fonction FindAll() pour tester la connexion à la BDD

--- FanTastyBack/Models/Ingredient.cs
+++ FanTastyBack/Models/Ingredient.cs
@@ -1,6 +1,27 @@
-﻿namespace FanTastyBack.Models
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Collections.Generic;
+
+namespace FanTastyBack.Models
 {
     public class Ingredient
     {
+
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        
+        [BsonElement("nom")]
+        public string Nom { get; set; }
+        
+        [BsonElement("image")]
+        public string Image { get; set; }
+        
+        public Ingredient(string id, string nom, string image, List<string> unites)
+        {
+            Id = id;
+            Nom = nom;
+            Image = image;
+        }
     }
 }
--- FanTastyBack/Repositories/Interfaces/IIngredientRepository.cs
+++ FanTastyBack/Repositories/Interfaces/IIngredientRepository.cs
@@ -1,7 +1,10 @@
-﻿namespace FanTastyBack.Repositories.Interfaces
+﻿using FanTastyBack.Models;
+using System.Collections.Generic;
+
+namespace FanTastyBack.Repositories.Interfaces
 {
     public interface IIngredientRepository
     {
-
+        public List<Ingredient> FindAll();
     }
 }
