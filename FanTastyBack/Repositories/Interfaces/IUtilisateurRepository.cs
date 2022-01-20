94396494bdd516f95ddcac4d04272697c255fa1c
timothée <timothee.lafay.pro@gmail.com>
2022-01-20 11:06:58 +0100

    Ajout method FindAll() pour les utilisateurs

--- FanTastyBack/Repositories/Interfaces/IUtilisateurRepository.cs
+++ FanTastyBack/Repositories/Interfaces/IUtilisateurRepository.cs
@@ -1,6 +1,11 @@
-﻿namespace FanTastyBack.Repositories.Interfaces
+﻿using FanTastyBack.Models;
+using System.Collections.Generic;
+
+namespace FanTastyBack.Repositories.Interfaces
 {
     public interface IUtilisateurRepository
     {
+        public List<Utilisateur> FindAll();
+
     }
 }
--- FanTastyBack/Repositories/UtilisateurRepository.cs
+++ FanTastyBack/Repositories/UtilisateurRepository.cs
@@ -1,8 +1,27 @@
-﻿using FanTastyBack.Repositories.Interfaces;
+﻿using FanTastyBack.Config;
+using FanTastyBack.Models;
+using FanTastyBack.Repositories.Interfaces;
+using MongoDB.Driver;
+using System.Collections.Generic;
 
 namespace FanTastyBack.Repositories
 {
     public class UtilisateurRepository : IUtilisateurRepository
     {
+        private readonly IMongoCollection<Utilisateur> _utilisateur;
+
+        public UtilisateurRepository(IFantastyDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+            _utilisateur = database.GetCollection<Utilisateur>(settings.UtilisateursCollectionName);
+        }
+
+        public List<Utilisateur> FindAll()
+        {
+            List<Utilisateur> utilisateurs = this._utilisateur.Find(ingr => true).ToList();
+            return utilisateurs;
+        }
+
     }
 }
