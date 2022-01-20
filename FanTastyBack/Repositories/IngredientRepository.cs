0005516a13d23c6c6f3dc2bf0e506691420ab159
Audrey Serra <audrey.serra@free.fr>
2022-01-20 11:28:31 +0100

    Ajout CRUD IngredientRepository

--- FanTastyBack/Repositories/IngredientRepository.cs
+++ FanTastyBack/Repositories/IngredientRepository.cs
@@ -17,6 +17,7 @@ namespace FanTastyBack.Repositories
             _ingredients = database.GetCollection<Ingredient>(settings.IngredientsCollectionName);
         }
 
+
         public List<Ingredient> FindAll()
         {
             List<Ingredient> ingredients = this._ingredients.Find(ingr => true).ToList();
@@ -26,6 +27,29 @@ namespace FanTastyBack.Repositories
         public Ingredient FindById(string id)
         {
             Ingredient ingredient = this._ingredients.Find(ingr => ingr.Id == id).FirstOrDefault();
+            return ingredient;
+        }
+
+        public Ingredient FindByName(string nom)
+        {
+            Ingredient ingredient = this._ingredients.Find(ingr => ingr.Nom == nom).FirstOrDefault();
+            return ingredient;
+        }
+        public Ingredient Create(Ingredient ingredient)
+        {
+            _ingredients.InsertOne(ingredient);
+            return ingredient;
+        }
+
+        public void Delete(string id)
+        {
+            _ingredients.DeleteOne(ingr => ingr.Id == id);
+        }
+
+        public Ingredient Update(string id, Ingredient ingredient)
+        {
+            ingredient.Id = id;
+            _ingredients.ReplaceOne(ingr => ingr.Id == id, ingredient);
             return ingredient;
         }
     }
