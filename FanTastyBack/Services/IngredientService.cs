b4e53a08735dc5315d24eee7a98c905cc9fd7bca
Audrey Serra <audrey.serra@free.fr>
2022-01-20 14:11:24 +0100

    Ajout CRUD IngredientService

--- FanTastyBack/Services/IngredientService.cs
+++ FanTastyBack/Services/IngredientService.cs
@@ -1,4 +1,5 @@
-﻿using FanTastyBack.Models;
+﻿using FanTastyBack.Exceptions;
+using FanTastyBack.Models;
 using FanTastyBack.Repositories.Interfaces;
 using FanTastyBack.Services.Interfaces;
 using System.Collections.Generic;
@@ -17,13 +18,46 @@ namespace FanTastyBack.Services
         public List<Ingredient> FindAll()
         {
             List<Ingredient> ingredients = this._repository.FindAll();
+            if(ingredients == null || ingredients.Count == 0)
+            {
+                throw new NotFoundException("Not found.");
+            }
             return ingredients;
         }
 
         public Ingredient FindById(string id)
         {
             Ingredient ingredient = this._repository.FindById(id);
+            if(ingredient == null)
+            {
+                throw new NotFoundException("Not found.");
+            }
             return ingredient;
+        }
+
+        public Ingredient FindByName(string nom)
+        {
+            Ingredient ingredient = this._repository.FindByName(nom);
+            if (ingredient == null)
+            {
+                throw new NotFoundException("Not found.");
+            }
+            return ingredient;
+        }
+
+        public Ingredient Update(string id, Ingredient ingredient)
+        {
+            return this._repository.Update(id, ingredient);
+        }
+        
+        public Ingredient Create(Ingredient ingredient)
+        {
+            return this._repository.Create(ingredient);
+        }
+
+        public void Delete(string id)
+        {
+            this._repository.Delete(id);
         }
     }
 }
