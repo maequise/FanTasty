9ac4de1d26112797eb19049de9e6cca240695630
maequise <tahitoa.merlin@gmail.com>
2022-03-05 19:41:16 +0100

    TECH: Edit the repository to mix the different filters used

--- FanTastyFront/src/app/services/recettes.service.ts
+++ FanTastyFront/src/app/services/recettes.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Recette } from '../models/recette';
@@ -35,7 +35,13 @@ export class RecettesService {
   }
 
   findByTag(tag : Tag) : Observable<Recette[]>{
-      return this.httpClient.get<Recette[]>(Constants.URL_BACK + '/api/recettes/tag/' + JSON.stringify(tag))
+    console.log(tag);
+
+    let params = new HttpParams().set('tagString', JSON.stringify(tag));
+
+      return this.httpClient.get<Recette[]>(Constants.URL_BACK + '/api/recettes/tag-search', {
+          params: params
+        });
   }
 
   getImage(filename : string) : string{
